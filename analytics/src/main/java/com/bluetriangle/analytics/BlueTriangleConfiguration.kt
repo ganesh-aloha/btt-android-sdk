498e7da93c5d872d2f3a3c717f3d36654e729c24
Ganesh <ganesh@alohatechnology.com>
2026-07-13 20:41:46 +0530

    Logcat Logs not showing in Debug Mode

--- analytics/src/main/java/com/bluetriangle/analytics/BlueTriangleConfiguration.kt
+++ analytics/src/main/java/com/bluetriangle/analytics/BlueTriangleConfiguration.kt
@@ -50,10 +50,16 @@ class BlueTriangleConfiguration {
     var shouldSampleNetwork = false
 
     var isDebug = false
+        set(value) {
+            field = value
+            if (!field)
+                logger = null
+        }
+
     var debugLevel = Log.DEBUG
     var logger: Logger? = null
         get() {
-            if (BuildConfig.DEBUG && field == null && isDebug) {
+            if (field == null && isDebug) {
                 field = AndroidLogger(debugLevel)
             }
             return field
